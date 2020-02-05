8cf22e7e80e12575ef470cb03b5241d6a285acdf
valeriegamon <valeriegamon@valeries-mbp.ddns.creighton.edu>
2020-02-05 16:19:24 -0600

    update to first branch
    up to event 4

--- forms/pets/pets.js
+++ forms/pets/pets.js
@@ -3,4 +3,23 @@ let withMarmaduke = ['Darcy', 'Jack', 'Jerry', 'Marty', 'Vito', 'Vinny', 'Riley'
 
 console.log(petNames.length);
 console.log(withMarmaduke.length);
-console.log(withMarmaduke.length);
+
+
+//remove "Vinny" from the array
+let removeVinny = petNames.splice(5, 1);
+console.log(petNames.length);
+
+
+//remove "Marty" from the array.
+let removeMarty = petNames.splice(3, 1);
+console.log(petNames.length);
+
+
+//add "Nancy" to the front of the array.
+petNames.unshift('Nancy');
+console.log(petNames.length);
+
+
+//add their name to the end of the array.
+petNames.unshift('Val');
+console.log(petNames.length);
